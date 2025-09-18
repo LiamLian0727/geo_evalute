a8842dd57044712d0d656e0ed01cca555a707e2f
Shijie Lian <1225419368@qq.com>
2025-09-18 17:29:03 +0800

    init

---
+++ geo_evalute/__init__.py
@@ -0,0 +1,47 @@
+from .prime_math import compute_score as prime_compute_score
+from .math_verify_comp import compute_score as math_verify_compute_score
+from .math_rel_comp import compute_score as rel_compute_score
+
+def norm_string(string):
+    # replace pi
+    string = string.replace("π", "\\pi")
+    
+    # replace tfrac and dfrac with frac
+    string = string.replace("tfrac", "frac")
+    string = string.replace("dfrac", "frac")
+
+    # remove \left and \right
+    string = string.replace("\\left", "")
+    string = string.replace("\\right", "")
+
+    # Remove circ (degrees)
+    string = string.replace("^{\\circ}", "")
+    string = string.replace("^\\circ", "")
+    return string
+
+def compute_score(model_output: str, ground_truth: str) -> bool:
+    
+    extracted_model_output = model_output
+    
+    try:
+        prime_score, _ , extracted_model_output = prime_compute_score(model_output, ground_truth)[0]
+        if prime_score:
+            return 1.0, extracted_model_output
+    except Exception as e:
+        prime_score = 0.0
+        
+    try:
+        math_verify_score, math_verify_extracted_model_output = math_verify_compute_score(norm_string(model_output), ground_truth)
+        if math_verify_score:
+            return 1.0, math_verify_extracted_model_output
+    except Exception as e:
+        math_verify_score = 0.0
+        
+    try:
+        rel_score = rel_compute_score(norm_string(extracted_model_output), ground_truth, rel_tol=0.01)
+        if rel_score:
+            return 1.0, extracted_model_output
+    except Exception as e:
+        rel_score = 0.0
+        
+    return 0.0, extracted_model_output
---
+++ geo_evalute/math_rel_comp.py
@@ -0,0 +1,47 @@
+import re
+import json
+from typing import Optional
+from tqdm import tqdm
+import math
+from sympy import Expr
+
+try:
+    from math_verify import parse 
+except ImportError:
+    print("To use Math-Verify, please install it first by running `pip install math-verify`.")
+
+
+def _to_float(expr):
+    if isinstance(expr, Expr):
+        return float(expr.evalf())
+    return float(expr)
+
+def compute_score(pred: str, gt: str, rel_tol: float = 0.01) -> bool:
+    
+    
+    try:
+        p_val = float(pred)
+        g_val = float(gt)
+        return math.isclose(p_val, g_val, rel_tol=rel_tol)
+    except (Exception):
+        pass
+    
+    try:
+        p_exprs = parse(pred)
+        g_exprs = parse(gt)
+
+        p = p_exprs[0] if p_exprs else None
+        g = g_exprs[0] if g_exprs else None
+        if p is None or g is None:
+            return False
+
+        p_val, g_val = _to_float(p), _to_float(g)
+        return math.isclose(p_val, g_val, rel_tol=rel_tol)
+
+    except Exception:
+        return False
+    
+    return False
+
+    
+
